804fa8086a779c650f9cf848cfefc4d6056b14ce
Yala4444 <georgy.dzr@gmail.com>
2026-09-17 21:55:24 +0300

    fix: explicitly bind WebRuntime world to mobile controls

--- scripts/autoload/web_runtime.gd
+++ scripts/autoload/web_runtime.gd
@@ -1,10 +1,11 @@
 extends CanvasLayer
 
-const BUILD_LABEL := "v0.9.2"
+const BUILD_LABEL := "v0.9.3"
 
 var version_label: Label
 var scan_timer: float = 0.0
 var active_world: GameWorld = null
+var last_bound_world: GameWorld = null
 
 func _ready() -> void:
     layer = 96
@@ -21,12 +22,21 @@ func _ready() -> void:
 func _process(delta: float) -> void:
     scan_timer -= delta
     if scan_timer <= 0.0:
-        scan_timer = 0.12
+        scan_timer = 0.10
         active_world = _find_world(get_tree().current_scene)
 
     var mobile_controls: Node = get_tree().root.get_node_or_null("MobileControls")
     if mobile_controls != null:
-        var should_force: bool = OS.has_feature("web") and active_world != null and is_instance_valid(active_world)
+        var has_world: bool = active_world != null and is_instance_valid(active_world)
+        if has_world:
+            if last_bound_world != active_world:
+                mobile_controls.call("bind_world", active_world)
+                last_bound_world = active_world
+        elif last_bound_world != null:
+            mobile_controls.call("unbind_world", last_bound_world)
+            last_bound_world = null
+
+        var should_force: bool = OS.has_feature("web") and has_world
         mobile_controls.call("force_visible_for_test", should_force)
 
     if version_label != null:
