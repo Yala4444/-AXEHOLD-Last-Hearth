4d7b4b26c5fedf7528b20812af0e124272ed1d5d
Yala4444 <georgy.dzr@gmail.com>
2026-09-19 00:31:55 +0300

    v1.13 visual reboot: redraw resource icons for small-screen readability

--- scripts/ui/resource_icon.gd
+++ scripts/ui/resource_icon.gd
@@ -5,7 +5,7 @@ var kind: String = "wood"
 
 func configure(value: String) -> void:
     kind = value
-    custom_minimum_size = Vector2(13, 13)
+    custom_minimum_size = Vector2(15,15)
     mouse_filter = Control.MOUSE_FILTER_IGNORE
     queue_redraw()
 
@@ -21,28 +21,39 @@ func _draw() -> void:
             _draw_wood()
 
 func _draw_wood() -> void:
-    draw_rect(Rect2(1, 8, 11, 3), Color("3b2a1d"))
-    draw_rect(Rect2(2, 5, 9, 5), Color("7b5130"))
-    draw_rect(Rect2(3, 4, 7, 2), Color("9d6b3e"))
-    draw_rect(Rect2(4, 6, 2, 2), Color("c08a51"))
-    draw_rect(Rect2(8, 6, 2, 2), Color("c08a51"))
+    draw_colored_polygon(PackedVector2Array([
+        Vector2(1,9),Vector2(4,4),Vector2(12,3),Vector2(14,7),Vector2(11,12),Vector2(3,13)
+    ]),Color("5c3b26"))
+    draw_colored_polygon(PackedVector2Array([
+        Vector2(3,8),Vector2(5,5),Vector2(11,4),Vector2(12,7),Vector2(10,10),Vector2(4,11)
+    ]),Color("8d5c35"))
+    draw_line(Vector2(4,7),Vector2(11,6),Color("bc8650"),1.2)
+    draw_line(Vector2(5,10),Vector2(9,9),Color("6d462b"),1.0)
 
 func _draw_stone() -> void:
-    draw_rect(Rect2(2, 9, 9, 2), Color("596161"))
-    draw_rect(Rect2(1, 6, 11, 4), Color("87908f"))
-    draw_rect(Rect2(3, 3, 6, 3), Color("a8b2b0"))
-    draw_rect(Rect2(5, 4, 2, 2), Color("c4ccca"))
+    draw_colored_polygon(PackedVector2Array([
+        Vector2(1,10),Vector2(3,5),Vector2(7,2),Vector2(12,4),Vector2(14,9),Vector2(10,13),Vector2(4,13)
+    ]),Color("687270"))
+    draw_colored_polygon(PackedVector2Array([
+        Vector2(4,6),Vector2(7,3),Vector2(11,5),Vector2(10,9),Vector2(5,10)
+    ]),Color("97a19f"))
+    draw_line(Vector2(5,5),Vector2(8,7),Color("c7cfcd"),1.0)
 
 func _draw_ore() -> void:
-    draw_rect(Rect2(1, 8, 11, 3), Color("55435f"))
-    draw_rect(Rect2(2, 5, 9, 5), Color("75558d"))
-    draw_rect(Rect2(4, 2, 5, 4), Color("a978c5"))
-    draw_rect(Rect2(5, 3, 3, 2), Color("d1a6e8"))
-    draw_rect(Rect2(9, 6, 2, 2), Color("bc8bd8"))
+    draw_colored_polygon(PackedVector2Array([
+        Vector2(1,11),Vector2(3,5),Vector2(7,2),Vector2(13,5),Vector2(14,10),Vector2(10,13),Vector2(4,13)
+    ]),Color("584466"))
+    draw_colored_polygon(PackedVector2Array([
+        Vector2(5,10),Vector2(6,4),Vector2(9,2),Vector2(11,6),Vector2(9,11)
+    ]),Color("9c6fba"))
+    draw_line(Vector2(8,4),Vector2(8,9),Color("deb9ef"),1.3)
+    draw_circle(Vector2(11,7),1.5,Color("c995e2"))
 
 func _draw_part() -> void:
-    draw_circle(Vector2(6.5, 6.5), 5.2, Color("5a5f61"))
-    draw_circle(Vector2(6.5, 6.5), 3.4, Color("90989a"))
-    draw_circle(Vector2(6.5, 6.5), 1.5, Color("303638"))
-    for p: Vector2 in [Vector2(5.5,0), Vector2(5.5,10), Vector2(0,5.5), Vector2(10,5.5)]:
-        draw_rect(Rect2(p, Vector2(3,3)), Color("b2b9b8"))
+    draw_circle(Vector2(7.5,7.5),5.6,Color("555d60"))
+    for i: int in range(8):
+        var a: float = TAU*float(i)/8.0
+        var p := Vector2(7.5,7.5)+Vector2(cos(a),sin(a))*5.8
+        draw_rect(Rect2(p-Vector2(1.2,1.2),Vector2(2.4,2.4)),Color("879194"))
+    draw_circle(Vector2(7.5,7.5),3.5,Color("a6afb0"))
+    draw_circle(Vector2(7.5,7.5),1.6,Color("30383b"))
